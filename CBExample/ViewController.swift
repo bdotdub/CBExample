f27fadcfa16e0ac14c606645ee1fe7646027103e
Benny Wong <benny@bwong.net>
2015-11-03 10:56:01 -0500

    Add CBCentralManager example

--- CBExample/ViewController.swift
+++ CBExample/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var centralManager: CBCentralManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.centralManager = CBCentralManager(delegate: self, queue: nil)
+        self.centralManager?.scanForPeripheralsWithServices(nil, options: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // mark: CBCentralManagerDelegate
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        NSLog("Connected peripheral \(peripheral.name)")
     }
-
-
+    
+    func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        NSLog("Found peripheral \(peripheral.name)")
+    }
+    
+    func centralManager(central: CBCentralManager, didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        NSLog("Failed to connect to peripheral \(peripheral.name)")
+    }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager) {
+        switch central.state {
+        case .PoweredOff:
+            NSLog("Central Manager: Powered off")
+        case .PoweredOn:
+            NSLog("Central Manager: Powered on")
+        case .Resetting:
+            NSLog("Central Manager: Resetting")
+        case .Unauthorized:
+            NSLog("Central Manager: Unauthorized")
+        case .Unknown:
+            NSLog("Central Manager: Unknown")
+        case .Unsupported:
+            NSLog("Central Manager: Unsupported")
+        }
+    }
+    
+    
+    // mark: CBCentralPeripheralDelegate
+    func peripheral(peripheral: CBPeripheral, didDiscoverServices error: NSError?) {
+        NSLog("discover services")
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didDiscoverCharacteristicsForService service: CBService, error: NSError?) {
+    }
+    
+    func peripheral(peripheral: CBPeripheral, didUpdateValueForCharacteristic characteristic: CBCharacteristic, error: NSError?) {
+    }
 }
-
